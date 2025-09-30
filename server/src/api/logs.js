4da07a570f2b3b48288b3bf38724f8752b3875a8
darth.revan.030 <smartsamarth03@gmail.com>
2025-10-01 01:29:02 +0800

    feat: refactor logs API to implement CRUD operations and improve error handling

--- server/src/api/logs.js
+++ server/src/api/logs.js
@@ -3,18 +3,57 @@ const LogEntry = require('../models/LogEntry');
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.json({
-        message: '🌐',
-    })
+
+// create a new log
+router.post('/', async (req, res, next) => {
+    try {
+        const logEntry = new LogEntry(req.body);
+        const createdEntry = await logEntry.save();
+        res.json(createdEntry);
+    } catch (error){
+        if (error.name === 'ValidationError'){
+            res.status(422);
+        }
+        next(error);
+    }
 });
 
-router.post('/', (req, res) => {
-    const LogEntry = new LogEntry(req.body);
-    console.log(req.body)
-    res.json({
-        message: "Log Posted",
-    })
+
+// update an entry
+router.put('/', async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        next(error);
+    }
+});
+
+// get all the entries
+router.get('/', async (req, res, next) => {
+    try {
+        const logEntries = await LogEntry.find({});
+        res.json(logEntries);
+    } catch (error) {
+        // if (error.name === 'ValidationError'){
+        //     res.status(422);
+        // }
+        next(error);
+    }
+    
+});
+
+
+// delete entry
+router.delete('/:id', async (req, res, next) => {
+    try {
+        const deletedLog = await LogEntry.findByIdAndDelete(req.params.id);
+        if (!deletedLog){
+            return res.status(404).json({ message: "Log Entry Not Found" });
+        }
+        res.json(deletedLog);
+    } catch (error) {
+        next(error);
+    }
 });
 
 module.exports = router;
