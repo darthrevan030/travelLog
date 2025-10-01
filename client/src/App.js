1a288591f279d788c04b8e330c468692e286a375
darth.revan.030 <smartsamarth03@gmail.com>
2025-10-01 21:02:35 +0800

    feat: implement map functionality with Leaflet; replace App.css with styles.css and add custom marker icon

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles.css';
+import "leaflet/dist/leaflet.css";
 
-function App() {
+import { useState, useEffect } from 'react';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { Icon } from 'leaflet';
+
+
+export default function App() {
+
+  // state to store travel logs
+  const [logs, setLogs] = useState([]);
+
+  // useEffect runs when component loads
+  useEffect(() => {
+    // define async function to fetch data
+    const fetchLogs = async () => {
+      try {
+        // Step 1: make request to backend
+        const response = await fetch('http://localhost:1337/api/logs');
+        // Step 2: Convert response data to json
+        const data = await response.json();
+        // Step 3: update state
+        setLogs(data);
+        
+        // log the response data
+        console.log("🚀 ~ fetchLogs ~ data:", data)
+    
+      } catch (error) {
+        // Step 4: Error Handling
+        console.error('Error fetching logs: ', error);
+      } 
+    };
+
+    // call the function
+    fetchLogs();
+  }, []); // empty array for 2nd parameter means "run once when component loads"
+
+  const customIcon = new Icon({
+    iconUrl: "/icons/marker-icon.png",
+    iconSize: [38, 38] // size of icon
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <MapContainer center={[1.351538170692347, 103.80731437210247]} zoom={12}>
+      <TileLayer
+        attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+      />
+
+      {/* loop through fetched logs and create marker for each */}
+      {logs.map(log => (
+        <Marker
+          icon={customIcon}
+          key={log._id}
+          position={[log.latitude, log.longitude]}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Popup>
+            {log.title}
+          </Popup>
+
+        </Marker>
+      ))}
+    </MapContainer>
   );
 }
-
-export default App;
